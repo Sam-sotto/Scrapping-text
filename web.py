d38685e2d66ce11e8bda34955643cf869584c7e3
Samu <58440642+samu-hub@users.noreply.github.com>
2020-04-16 05:40:50 -0300

    Update web.py

--- web.py
+++ web.py
@@ -5,11 +5,7 @@ page = requests.get('https://forecast.weather.gov/MapClick.php?lat=34.0534900000
 soup = BeautifulSoup(page.content, 'html.parser')
 week = soup.find(id="seven-day-forecast-body")
 
-#week2 = week.find_all(class_="tombstone-container")
 items = week.find_all(class_="tombstone-container")
-
-print(items[0].find(class_="period-name").get_text())
-
 
 for item in items:
     print(item.get_text())
